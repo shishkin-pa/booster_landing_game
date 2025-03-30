d7decc9bed682aacf9913679f5f9ea154a476f5a
shipopala <shishkin.pa@phystech.edu>
2025-03-30 20:47:51 +0300

    V0.2.1

--- Platform.cpp
+++ Platform.cpp
@@ -1,19 +1,23 @@
 #include "Platform.h"
 
+// Конструктор платформы
 Platform::Platform(float x, float y) {
-    shape.setSize(sf::Vector2f(200, 20)); // Размер платформы
-    shape.setFillColor(sf::Color::Blue); // Цвет платформы
-    shape.setPosition(x, y); // Позиция платформы
+    shape.setSize(sf::Vector2f(200, 20)); // Установка размера платформы (ширина 200, высота 20)
+    shape.setFillColor(sf::Color::Blue);  // Установка синего цвета платформы
+    shape.setPosition(x, y);              // Установка позиции платформы
 }
 
+// Отрисовка платформы в окне
 void Platform::draw(sf::RenderWindow& window) {
-    window.draw(shape);
+    window.draw(shape); // Отрисовка прямоугольника платформы
 }
 
+// Получение границ платформы для проверки коллизий
 sf::FloatRect Platform::getBounds() const {
-    return shape.getGlobalBounds();
+    return shape.getGlobalBounds(); // Возвращает глобальные границы прямоугольника
 }
 
+// Установка новой позиции платформы
 void Platform::setPosition(float x, float y) {
-    shape.setPosition(x, y);
+    shape.setPosition(x, y); // Устанавливает новую позицию прямоугольника
 }
